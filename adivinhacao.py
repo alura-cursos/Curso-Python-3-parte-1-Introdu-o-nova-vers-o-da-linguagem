d0309034af5292a638c14952964f98e488f7f9c6
Andre Chaves <andrechavesg@gmail.com>
2017-12-26 14:09:57 -0200

    capitulo6

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,12 +1,15 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
