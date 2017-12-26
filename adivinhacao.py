eafa6fb7a934d6b28c7993538a846ea4daf936d0
Andre Chaves <andrechavesg@gmail.com>
2017-12-26 14:04:11 -0200

    capitulo3

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,15 +2,23 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
+
 numero_secreto = 42
 
 chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
+print("Você digitou " , chute_str)
 chute = int(chute_str)
 
-if (numero_secreto == chute):
-    print("Você acertou!")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Parabéns! Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("O seu chute foi maior do que o número secreto!")
+    elif(menor):
+        print("O seu chute foi menor do que o número secreto!")
 
 print("Fim do jogo")
